3da3ba361dc54537022f13550561be8a9ab283b4
吉澤勇作 <>
2022-03-14 16:21:25 +0900

    削除テスト 成功

--- tests/Feature/Api/TodoControllerTest.php
+++ tests/Feature/Api/TodoControllerTest.php
@@ -89,7 +89,7 @@ class TodoControllerTest extends TestCase
         Todo::factory()->count(1)->create();
         $todos = Todo::all();
         $data = $todos->first();
-        $id = 1;
+        $id = $data->id;
         $res = $this->postJson(route('api.todo.delete',['id'=>$id]));
         $res->assertOk();
 
