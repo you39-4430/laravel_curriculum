312f243bcf6dfbcfb1eb205fb70b6bc887fe3375
吉澤勇作 <>
2022-03-16 11:01:58 +0900

    更新テスト 成功

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -33,6 +33,23 @@ class TodoController extends Controller
         return ['message' => 'ok'];
     }
 
-
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $validated = $request->validate([
+            'title' => ['required', 'string', 'max:255'],
+            'content' => ['required', 'string', 'max:255']
+        ]);
+        $this->todo->findOrFail($id)->fill($validated)->update();
+        $todo = $this->todo->findOrFail($id);
+        return $todo;
+    }
 
 }
